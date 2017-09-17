fb40659c69b376a73a43b3d26446795d94b2e10e
Aurélio Vinícios <aurelio.vinicios1996@gmail.com>
2017-09-17 16:09:05 -0300

    update top-top challenge

--- desafio.js
+++ desafio.js
@@ -3,17 +3,20 @@ const knex = require('knex')(knexfile)
 const axios = require("axios")
 
 const api = axios.create({
-  baseURL: "https://api.github.com"
+    baseURL: "https://api.github.com"
 })
 
 api.get("/repos/seita-ifce/hello-js-v3/issues/3/comments").then(ret => {
     let commentsList = ret.data.filter(comment => new Date(comment.created_at) <= new Date('2017-09-18T23:59:59'))
+    console.log('Dados da issue recuperados')
     commentsList.forEach(e => {
         if (e.body.trim().startsWith('https://github.com/')) {
             knex('presenca').select().where({
                 usuario: e.user.login
             }).then((ret) => {
-                if (ret.length == 0) {
+                console.log('procurando duplicatas')
+                if (ret.length == 0) {        
+                    console.log(`novo registro.`)
                     knex('presenca').insert({
                         usuario: e.user.login,
                         repo: e.body.trim(),
@@ -21,18 +24,17 @@ api.get("/repos/seita-ifce/hello-js-v3/issues/3/comments").then(ret => {
                         date: new Date(e.created_at)
                     }).then(() => {
                         console.log(`${e.user.login} completou o desafio.`)
-                        process.exit(1)
                     }).catch((err) => {
                         console.log(err)
                         process.exit(0)
                     })
                 } else {
-                    process.exit(1)
+                    console.log(`registro ja inserido`)
                 }
             }).catch((err) => {
                 console.log(err)
                 process.exit(0)
-    })
+            })
         }
     });
 })
